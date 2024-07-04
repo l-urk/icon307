2beaccdad1a78d374c8cafacb2ca6a19b41344b1
L Kira <112792604+l-urk@users.noreply.github.com>
2024-07-04 06:11:13 -0700

    Update icon307.bat

--- icon307.bat
+++ icon307.bat
@@ -1,4 +1,4 @@
 echo y| reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons"
-echo y| reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons" /v "3" /t REG_SZ /d "C:\icon307" /f
-echo y| reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons" /v 4 /t REG_SZ /d "C:\icon307" /f
+echo y| reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons" /v 3 /t REG_SZ /d "C:\icon307.dll" /f
+echo y| reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons" /v 4 /t REG_SZ /d "C:\icon307.dll" /f
 pause
